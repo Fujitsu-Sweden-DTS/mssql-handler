51fc1060e948da2347a9ee0b7241e897c28aad7f
Axel Svensson <mail@axelsvensson.com>
2021-05-24 16:36:20 +0200

    First commit: Boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+"use strict";
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "@fujitsusweden/standard",
+  parserOptions: { ecmaVersion: 12 },
+  rules: {},
+};
